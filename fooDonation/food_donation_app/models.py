549ca41623ba779cfb93f57cc315a59722fa94e6
Jyoti <jyotisingh82000@gmail.com>
2023-09-02 16:50:28 +0530

    media file added

--- fooDonation/food_donation_app/models.py
+++ fooDonation/food_donation_app/models.py
@@ -8,6 +8,7 @@ class FoodDetail(models.Model):
     food_name = models.CharField(max_length=40, default='')
     description_food = models.CharField(max_length=100, default='')
     date_delivery = models.DateField(auto_created=True)
+    image = models.ImageField(upload_to="shop/images", default="")
     fresh = models.BooleanField('fresh', default='not fresh')
 
     def __str__(self):
